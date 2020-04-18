3b6e6482b61f98f56b62f1a1a963fd7c91e4ddfb
Peter Anderson <pete.anderson@gmail.com>
2020-04-18 20:19:39 +0200

    attack almost finished

--- components/Attack-Area.vue
+++ components/Attack-Area.vue
@@ -2,29 +2,38 @@
 	<div class="battle-area">
         <h1>{{getUnitThatCanAttack[0].name}} v {{getUnitUnderAttack[0].name}}</h1>
         
-        <div class="attacker">
-            <h2>{{getUnitThatCanAttack[0].name}}</h2>
-            <div v-for="(die, i) in attackingDie" :key="i" :class="[die.rolled ? '' : 'not-rolled']">
-                <img class="dice-face" src="/skull.png" alt="">
-                <img class="dice-face" src="/skull.png" alt="">
-                <img class="dice-face" src="/skull.png" alt="">
-                <img class="dice-face" src="/shield.png" alt="">
-                <img class="dice-face" src="/blank.png" alt="">
-                <img class="dice-face" src="/blank.png" alt="">
+        <div class="dice-container">
+            <div class="attacker">
+                <h2>{{getUnitThatCanAttack[0].name}}</h2>
+                <div v-for="(die, i) in attackingDie" :key="i" class="die-container">
+                    <img class="dice-face" src="/skull.png" alt="" :class="[dieRolled(die.rolled, 0, die.value) ? 'user-rolled' : 'not-rolled']">
+                    <img class="dice-face" src="/skull.png" alt="" :class="[dieRolled(die.rolled, 1, die.value) ? 'user-rolled' : 'not-rolled']">
+                    <img class="dice-face" src="/skull.png" alt="" :class="[dieRolled(die.rolled, 2, die.value) ? 'user-rolled' : 'not-rolled']">
+                    <img class="dice-face" src="/shield.png" alt="" :class="[dieRolled(die.rolled, 3, die.value) ? 'user-rolled' : 'not-rolled']">
+                    <img class="dice-face" src="/blank.png" alt="" :class="[dieRolled(die.rolled, 4, die.value) ? 'user-rolled' : 'not-rolled']">
+                    <img class="dice-face" src="/blank.png" alt="" :class="[dieRolled(die.rolled, 5, die.value) ? 'user-rolled' : 'not-rolled']">
+                    <div class="roll-button" @click="rollDie('attackingDie', i)">ROLL!</div>
+                </div>
             </div>
-        </div>
 
-        <div class="defender">
-            <h2>{{getUnitUnderAttack[0].name}}</h2>
-            <div v-for="(die, i) in defendingDie" :key="i" @click="rollDie('defendingDie', i)">
-                <img class="dice-face" src="/skull.png" alt="" :class="[dieRolled(die.rolled, 0, die.value) ? 'user-rolled' : '']" >
-                <img class="dice-face" src="/skull.png" alt="" :class="[dieRolled(die.rolled, 0, die.value) ? 'user-rolled' : '']" >
-                <img class="dice-face" src="/skull.png" alt="" :class="[dieRolled(die.rolled, 0, die.value) ? 'user-rolled' : '']" >
-                <img class="dice-face" src="/shield.png" alt="" :class="[dieRolled(die.rolled, 0, die.value) ? 'user-rolled' : '']" >
-                <img class="dice-face" src="/blank.png" alt="" :class="[dieRolled(die.rolled, 0, die.value) ? 'user-rolled' : '']" >
-                <img class="dice-face" src="/blank.png" alt="" :class="[dieRolled(die.rolled, 0, die.value) ? 'user-rolled' : '']" >
+            <div class="defender">
+                <h2>{{getUnitUnderAttack[0].name}}</h2>
+                <div v-for="(die, i) in defendingDie" :key="i" class="die-container">
+                    <img class="dice-face" src="/skull.png" alt="" :class="[dieRolled(die.rolled, 0, die.value) ? 'user-rolled' : 'not-rolled']" >
+                    <img class="dice-face" src="/skull.png" alt="" :class="[dieRolled(die.rolled, 1, die.value) ? 'user-rolled' : 'not-rolled']" >
+                    <img class="dice-face" src="/skull.png" alt="" :class="[dieRolled(die.rolled, 2, die.value) ? 'user-rolled' : 'not-rolled']" >
+                    <img class="dice-face" src="/shield.png" alt="" :class="[dieRolled(die.rolled, 3, die.value) ? 'user-rolled' : 'not-rolled']" >
+                    <img class="dice-face" src="/blank.png" alt="" :class="[dieRolled(die.rolled, 4, die.value) ? 'user-rolled' : 'not-rolled']" >
+                    <img class="dice-face" src="/blank.png" alt="" :class="[dieRolled(die.rolled, 5, die.value) ? 'user-rolled' : 'not-rolled']" >
+                    <div class="roll-button" @click="rollDie('defendingDie', i)">ROLL!</div>
+                </div>
             </div>
         </div>
+       
+
+        <h2>Total Damage Dealt!: {{damageDealt}}</h2>
+
+        <div class="roll-button" v-if="allDieRolled">Turn finished! Click to continue</div>
         
     </div>
 </template>
@@ -36,7 +45,9 @@ export default {
     data() {
 		return {
             attackingDie: [],
-            defendingDie: []
+            defendingDie: [],
+            damageDealt: 0,
+            allDieRolled: false
         };
     },
     mounted: function() {
@@ -53,26 +64,53 @@ export default {
                 value:  Math.floor(Math.random() * 5)
             });
         }
-        console.log('this.defendingDie: ',this.defendingDie);
     },
     methods: {
         dieRolled: function(rolled, num, value) {
             return rolled && num === value;
         },
+        checkIfAllDieRolled: function() {
+            if (this.attackingDie.filter(die => !die.rolled).length + 
+                this.defendingDie.filter(die => !die.rolled).length === 0) {
+                this.allDieRolled = true;
+            }
+            
+        },
+        damageUpdate: function() {
+            const totalDamage = this.attackingDie.filter(die => die.rolled && die.value <= 2).length;
+            const totalDefense = this.defendingDie.filter(die => die.rolled && die.value === 3).length;
+            this.damageDealt = totalDamage - totalDefense;
+        },
         rollDie: function(die, i) {
-            console.log('die: ',die);
-            // dieToUpdate.map((die, idx) => {
-            //     if (i === idx) {
-            //         return {
-            //             ...die, 
-            //             rolled: true
-            //         }
-            //     } else {
-            //         return {
-            //             ...die
-            //         }
-            //     }
-            // })
+            if (die === "defendingDie") {
+                this.defendingDie = this.defendingDie.map((die, idx) => {
+                    if (i === idx) {
+                        return {
+                            ...die, 
+                            rolled: true
+                        }
+                    } else {
+                        return {
+                            ...die
+                        }
+                    }
+                })
+            } else {
+                this.attackingDie = this.attackingDie.map((die, idx) => {
+                    if (i === idx) {
+                        return {
+                            ...die, 
+                            rolled: true
+                        }
+                    } else {
+                        return {
+                            ...die
+                        }
+                    }
+                })
+            }
+            this.checkIfAllDieRolled();
+            this.damageUpdate();
         }
     },
     computed: {
@@ -95,16 +133,34 @@ export default {
     z-index: 500;
 }
 
+.dice-container {
+    display: flex;
+    justify-content: space-around;
+}
+
 .dice-face {
     height: 50px;
 }
 
 .not-rolled {
-    opacity: 0.5;
+    opacity: 0.2;
 }
 
 .user-rolled {
     opacity: 1;
+}
+
+.roll-button {
+    display: inline-block;
+    cursor: pointer;
+    font-size: 22px;
+    margin-left: 5px;
+    padding: 5px;
+    background-color: white;
+}
+
+.die-container {
+    display: flex;
 }
 
 </style>
